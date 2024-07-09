ece1145e93e661d045284a35c4a06c79b8289c3c
alicaank <alicaank14@gmail.com>
2024-07-09 20:41:27 +0200

    Remove print add argument

--- diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/__init__.py
@@ -95,8 +95,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         num_rendered = ctx.num_rendered
         raster_settings = ctx.raster_settings
         colors_precomp, language_feature_precomp,  means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer = ctx.saved_tensors
-        print("Backward")
-        print(language_feature_precomp)
+
         # Restructure args as C++ method expects them
         args = (raster_settings.bg,
                 means3D, 
@@ -112,6 +111,7 @@ class _RasterizeGaussians(torch.autograd.Function):
                 raster_settings.tanfovx, 
                 raster_settings.tanfovy, 
                 grad_out_color, 
+                grad_out_language_feature,
                 sh, 
                 raster_settings.sh_degree, 
                 raster_settings.campos,
@@ -120,8 +120,6 @@ class _RasterizeGaussians(torch.autograd.Function):
                 binningBuffer,
                 imgBuffer,
                 raster_settings.include_feature)
-        for i, arg in enumerate(args):
-            print(f"Type of args[{i}] ({arg}): {type(arg)}")
         # Compute gradients for relevant tensors by invoking backward method
         grad_means2D, grad_colors_precomp, grad_language_feature_precomp, grad_opacities, grad_means3D, grad_cov3Ds_precomp, grad_sh, grad_scales, grad_rotations = _C.rasterize_gaussians_backward(*args)        
 
